e7570eb9eba92638278b40a0a0eda69c1488fb62
Sungho Spark <spark.sungho@gmail.com>
2020-05-18 12:31:33 -0700

    brighter hero area and darken map

--- static/plugins/google-map/map.js
+++ static/plugins/google-map/map.js
@@ -5,6 +5,14 @@ window.marker = null; function initialize() {
     var mapMarker = $('#map').attr('data-marker');
     var mapMarkerName = $('#map').attr('data-marker-name');
     var nottingham = new google.maps.LatLng(latitude, longitude);
+    var style = [
+        { "featureType": "all", "elementType": "labels.text.fill", "stylers": [{ "saturation": 36 }, { "color": "#000000" }, { "lightness": 80 }] },
+        { "featureType": "all", "elementType": "labels.text.stroke", "stylers": [{ "visibility": "on" }, { "color": "#000000" }, { "lightness": 36 }] },
+        { "featureType": "all", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] },
+        { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#000000" }, { "lightness": 30 }] },
+        { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#000000" }, { "lightness": 37 },
+        { "weight": 1.2 }] },
+        { "featureType": "landscape", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 30 }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 21 }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#000000" }, { "lightness": 17 }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#000000" }, { "lightness": 29 }, { "weight": 0.2 }] }, { "featureType": "road.arterial", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 18 }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 16 }] }, { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 19 }] }, { "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#000000" }, { "lightness": 17 }] }];
     var mapOptions = { center: nottingham, mapTypeId: google.maps.MapTypeId.ROADMAP, backgroundColor: "#000", zoom: 14, panControl: !1, zoomControl: !0, mapTypeControl: !1, scaleControl: !1, streetViewControl: !1, overviewMapControl: !1, zoomControlOptions: { style: google.maps.ZoomControlStyle.LARGE } }
     map = new google.maps.Map(document.getElementById('map'), mapOptions); var mapType = new google.maps.StyledMapType(style, { name: "Grayscale" }); map.mapTypes.set('grey', mapType); map.setMapTypeId('grey'); var marker_image = mapMarker; var pinIcon = new google.maps.MarkerImage(marker_image, null, null, null, new google.maps.Size(46, 40)); marker = new google.maps.Marker({ position: nottingham, map: map, icon: pinIcon, title: mapMarkerName })
 }
